1c6813f7012925fa0ec4e02ce6a4eacbd1839d96
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-20 17:50:54 -0300

    refactor: :recycle: ajusta arquivo `server.ts` que tinha sido sobreescrito de forma incorreta

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,10 @@
-import { afterAll, beforeAll, test } from 'vitest'
-import request from 'supertest'
-import { app } from '../src/app'
+import { app } from './app'
+import { env } from './env'
 
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
-
-test('use can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
